4aa976164aa2eb9a377332bfaa36edf80851c23a
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2025-03-16 02:10:16 +0530

    title screen

--- src/Game.java
+++ src/Game.java
@@ -4,6 +4,12 @@ import java.awt.*;
 public class Game {
 
     JFrame window;
+    Container con;
+    JPanel titleNamePanel, startButtonPanel;
+    JLabel titleNameLabel;
+    Font titleFont = new Font("Times New Roman", Font.PLAIN, 70);
+    Font normalFont = new Font("Times New Roman", Font.PLAIN, 30);
+    JButton startButton;
 
     public static void main(String[] args) {
 
@@ -18,5 +24,29 @@ public class Game {
         window.getContentPane().setBackground(Color.black);
         window.setLayout(null);
         window.setVisible(true);
+        con = window.getContentPane();
+
+        titleNamePanel = new JPanel();
+        titleNamePanel.setBounds(100, 100, 600, 150);
+        titleNamePanel.setBackground(Color.black);
+        titleNameLabel = new JLabel("FORKED PATHS");
+        titleNameLabel.setForeground(Color.white);
+        titleNameLabel.setFont(titleFont);
+
+        startButtonPanel = new JPanel();
+        startButtonPanel.setBounds(290, 350, 200, 100);
+        startButtonPanel.setBackground(Color.black);
+
+        startButton = new JButton("START");
+        startButton.setBackground(Color.black);
+        startButton.setForeground(Color.white);
+        startButton.setFocusPainted(false);
+        startButton.setFont(normalFont);
+
+        titleNamePanel.add(titleNameLabel);
+        startButtonPanel.add(startButton);
+
+        con.add(titleNamePanel);
+        con.add(startButtonPanel);
     }
 }
