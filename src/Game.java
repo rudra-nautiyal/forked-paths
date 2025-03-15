602aed35da976b39d7679fb341bba6356d5d9459
rudra-nautiyal <rudra.x.nautiyal@gmail.com>
2025-03-16 01:03:31 +0530

    window initilized

---
+++ src/Game.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Game {
+
+    JFrame window;
+
+    public static void main(String[] args) {
+
+        new Game();
+    }
+
+    public Game() {
+
+        window = new JFrame();
+        window.setSize(800, 600);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().setBackground(Color.black);
+        window.setLayout(null);
+        window.setVisible(true);
+    }
+}
